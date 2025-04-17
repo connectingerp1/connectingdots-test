f931e9e6518d10e456751b899809df46a155a62d
connectingerp1 <connectingerp1@gmail.com>
2025-04-17 10:46:14 +0530

    Added missing components in sap, it and dv courses

--- src/app/(routes)/DV-Courses/page.js
+++ src/app/(routes)/DV-Courses/page.js
@@ -6,14 +6,20 @@ import Certificate from "@/components/HomePage/Certificate";
 import FAQ from "@/components/CoursesComponents/FAQ";
 import CoursesRelated from "@/components/CoursesComponents/RelatedCourses";
 import Description from "@/components/CoursesComponents/Description";
+import Counselor from "@/components/CoursesComponents/Councelor";
+import TrustUs from "@/components/CoursesComponents/Trustus";
+import Program from "@/components/CoursesComponents/ProgramHighlights";
 
 export default function DV() {
   return (
     <>
       <DSHeader pageId="DataVisualHeader" pageType="datavisualheader" />
       <Why pageId="WhyDataVisual" pageType="Whydatavisual" />
+      <Counselor />
       <Certificate pageId="DataVisual-CERT" />
+      <TrustUs />
       <Description pageId="DataVisual-landing" />
+      <Program />
       <FAQ pageId="DataVisualFAQ" pageType="datavisualfaq" />
       <CoursesRelated pageId="DataVisualrelcourses" />
     </>
--- src/app/(routes)/Sap-Courses/page.js
+++ src/app/(routes)/Sap-Courses/page.js
@@ -6,14 +6,21 @@ import Certificate from "@/components/HomePage/Certificate";
 import Description from "@/components/CoursesComponents/Description";
 import FAQ from "@/components/CoursesComponents/FAQ";
 import CoursesRelated from "@/components/CoursesComponents/RelatedCourses";
+import Counselor from "@/components/CoursesComponents/Councelor";
+import TrustUs from "@/components/CoursesComponents/Trustus";
+import Program from "@/components/CoursesComponents/ProgramHighlights";
+
 
 export default function HomePage() {
   return (
     <>
       <DSHeader pageId="SapHeader" pageType="sapheader" />
       <Why pageId="WhySap" pageType="Whysap" />
+      <Counselor />
       <Certificate pageId="sap-CERT" />
+      <TrustUs />
       <Description pageId="sap-landing" />
+      <Program />
       <FAQ pageId="SAPFAQ" pageType="sapfaq" />
       <CoursesRelated pageId="SAPrelcourses" />
     </>
