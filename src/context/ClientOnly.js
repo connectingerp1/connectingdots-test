395b3d739829de8a333a89d560eb34b64c4eee1e
connectingerp1 <connectingerp1@gmail.com>
2025-05-13 14:55:39 +0530

    debugging the issue

--- src/context/ClientOnly.js
+++ src/context/ClientOnly.js
@@ -1,8 +1,9 @@
-"use client"; // Mark this as a Client Component
+"use client"
 
 import { useEffect, useState } from "react";
 
-const ClientOnly = ({ children }) => {
+// This component ensures its children only render on the client side
+const ClientOnly = ({ children, key }) => {
   const [hasMounted, setHasMounted] = useState(false);
 
   useEffect(() => {
@@ -13,7 +14,10 @@ const ClientOnly = ({ children }) => {
     return null;
   }
 
-  return children;
+  // Apply the key to force re-rendering when it changes
+  return <div key={key}>
+    <div className="text-4xl bg-white my-2">This rendering but components are not rendering</div>
+    {children}</div>;
 };
 
 export default ClientOnly;
